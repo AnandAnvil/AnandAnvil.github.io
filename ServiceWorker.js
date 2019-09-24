eb6cc01f7899cdc6234d6fa002574c55fbbe60f5
Anand <anand@vizru.com>
2019-09-24 17:45:04 +0530

    dddddd

---
+++ ServiceWorker.js
@@ -0,0 +1,15 @@
+console.log("service worker loaded")
+self.addEventListener('install', (event) => {
+    console.log( 'install', event);
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log( 'activate', event);
+    return self.clients.claim();
+  });
+  
+  self.addEventListener('fetch', function(event) {
+     console.log( 'fetch', event);
+    event.respondWith(fetch(event.request));
+  });
