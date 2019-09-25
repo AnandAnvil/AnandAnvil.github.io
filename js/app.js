c8bd1447cdedae155f899586541f7e79903ffb40
Anand <anand@vizru.com>
2019-09-25 15:00:54 +0530

    notificatin status for granted message

--- js/app.js
+++ js/app.js
@@ -25,7 +25,13 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
 Notification.requestPermission(function(status) {
   console.log('Notification permission status:', status);
 });
-
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
 // const divInstall = document.getElementById('installContainer');
 // const butInstall = document.getElementById('butInstall');
 
