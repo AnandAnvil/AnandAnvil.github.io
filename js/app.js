c7a34185afcd4ebfe35e14ebb2604853735aaaa1
Anand <anand@vizru.com>
2019-09-25 15:02:39 +0530

    notificatin status for granted message message

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ Notification.requestPermission(function(status) {
   console.log('Notification permission status:', status);
 });
 function displayNotification() {
+  console.log('Notification permission status:::::::::::::::::::::::::::::::::::::::', status);
   if (Notification.permission == 'granted') {
     navigator.serviceWorker.getRegistration().then(function(reg) {
       reg.showNotification('Hello world!');
