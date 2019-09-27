a5b149f580ec184a1476fdb3402870f9834dacce
Anand <anand@vizru.com>
2019-09-27 14:45:07 +0530

    fetching api

--- js/app.js
+++ js/app.js
@@ -25,7 +25,10 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
   console.warn("Push messaging is not supported");
   console.log("Service workers are not supported.");
 }
-
+if (!('fetch' in window)) {
+  console.log('Fetch API not found, try including the polyfill');
+  return;
+}
 Notification.requestPermission(function(status) {
   console.log('Notification permission status:', status);
 });
